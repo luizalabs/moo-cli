eadf27e7b1a7b6619ca1b49a0e2c72751428f576
Lucas Vilaboim <lucas@vilaboim.com>
2019-12-03 15:19:44 -0300

    feat(git-hooks): add commit-msg hook to validate commig message using Angular Convention

--- config/git/verifyCommitMsg.js
+++ config/git/verifyCommitMsg.js
@@ -1,8 +1,6 @@
 const chalk = require('chalk')
-const msgPath = process.env.GIT_PARAMS
-const msg = require('fs')
-  .readFileSync(msgPath, 'utf-8')
-  .trim()
+const msgPath = process.env.HUSKY_GIT_PARAMS
+const msg = require('fs').readFileSync(msgPath, 'utf-8').trim()
 
 const commitRE = /^(revert: )?(build|feat|fix|docs|style|refactor|perf|test|workflow|ci|chore|types)(\(.+\))?: .{1,50}/
 
@@ -13,9 +11,8 @@ if (!commitRE.test(msg)) {
     chalk.red(`  Proper commit message format is required for automated changelog generation. Examples:\n\n`) +
     `    ${chalk.green(`feat(compiler): add 'comments' option`)}\n` +
     `    ${chalk.green(`options: (feat|fix|docs|style|refactor|perf|test|workflow|ci|chore|types)`)}\n` +
-    chalk.red(`  See .github/COMMIT_CONVENTION.md for more details.\n`)
-    // chalk.red(`  You can also use ${chalk.cyan(`npm run commit`)} to interactively generate a commit message.\n`)
+    chalk.red(`  See .github/COMMIT_CONVENTION.md for more details.\n`) +
+    chalk.red(`  You can also use ${chalk.cyan(`npm run commit`)} to interactively generate a commit message.\n`)
   )
-
   process.exit(1)
 }
